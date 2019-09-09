a8919f150aa55f36e7c9923261c56252303decdf
贾娜 <jiana511@126.com>
2019-09-09 12:02:52 +0800

    添加 demo

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+var path = require('path');
+
+module.exports = {
+  entry: '',
+  output: {
+    path: '',
+    filename: ''
+  },
+  devServer: {
+    // contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000
+  }
+}
