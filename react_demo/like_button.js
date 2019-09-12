0b35cc201a281a2e611101b653ed1a3724d3db8c
贾娜 <jiana511@126.com>
2019-09-12 10:42:32 +0800

    update

--- react_demo/like_button.js
+++ react_demo/like_button.js
@@ -40,6 +40,28 @@ class LikeButton extends React.Component {
 //     }
 // }
 
+// class clickMe extends React.Component {
+//    constructor(props){
+//      super(props);
+//      this.state = {
+//        count: 0
+//      }
+//    }
+//    clicked() {
+//      this.setState({
+//        count: this.state.count + 1
+//      })
+//    }
+//    render() {
+//      return (<div onClick = {
+//        () => this.clicked()
+//      }>点我{this.state.count}次</div>)
+//    }
+// }
+
 
 const domContainer = document.querySelector('#like_button_container');
 ReactDOM.render(e(LikeButton), domContainer);
+
+
+
